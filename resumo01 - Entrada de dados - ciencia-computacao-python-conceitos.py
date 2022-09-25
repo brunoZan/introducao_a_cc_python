cd78da770aad3eb9938561b7a2ed386a69a0ddca
Bruno_Duarte <bruno3xdk@gmail.com>
2022-09-24 22:12:50 -0300

    introdução_do_curso

---
+++ resumo01 - Entrada de dados - ciencia-computacao-python-conceitos.py
@@ -0,0 +1,40 @@
+Pequenos programas desemvolvidos durante o curso de Introdução a Ciencias da
+Computação com Python
+
+
+Verificar o tipo de váriavel 
+type(variavel);
+ numeros quebrados são float e nao number
+
+ Arredondar o resultado "//"
+ 10 // 3
+ o "//" arredonda o resultado
+
+ converter valores:
+ int(FLoat)
+ converte pra um inteiro
+ len() 
+ propriedade Lenght
+
+ Str()
+ propriedade String
+
+
+A função input() sempre devolve uma string
+temperatura = input("dígite uma temperatura")
+
+
+Expreções Booleanas
+
+operaor de comparação "END":
+   mode de usar: 3 < 4 end x ** 2 > 100
+nao se usa: &&
+
+operaor de comparação "OU":
+   mode de usar: 3 < 4 or x ** 2 > 100
+nao se usa: ||
+
+
+operaor de comparação "NEGAÇÃO":
+   mode de usar: not 3 < 4 
+nao se usa: !
