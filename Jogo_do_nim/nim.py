99519ccc958cd6a3d77e03f996a0807fe72800e4
Bruno_Duarte <bruno3xdk@gmail.com>
2022-10-10 17:56:48 -0300

    adicionada a função computador_escolhe_jogada

--- Jogo_do_nim/nim.py
+++ Jogo_do_nim/nim.py
@@ -1,7 +1,23 @@
-def computador_escolhe_jogada(n, m)
+# def computador_escolhe_jogada(n, m)
 
-def usuario_escolhe_jogada(n, m)
+# def usuario_escolhe_jogada(n, m)
 
-def partid()
+# def partida()
 
-def campeonato()
+# def campeonato()
+
+# partida
+# 	(pode au user o numero de deças e Tiradas)
+# 	(verifica quem começa baseado nas Tiradas)
+# 	(função: user_começa() ou pc_começa())
+	
+def computador_escolhe_jogada(n, m):
+	if n <= m:
+		return n
+	else:
+		quantia = n % (m + 1) 
+	if( quantia > 0):
+		return quantia
+	return m
+
+
